d23a4e17e796568c430dc281c8ae3a0596c3cf82
RyanHARRIS18 <ryanpaulharris18@gmail.com>
2020-05-14 11:13:59 -0600

    Edit button for profile added

---
+++ Views/Shared/_NavigationOfUserPartial.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConstellationWebApp.Views.Shared
+{
+    public class _NavigationOfUserPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
