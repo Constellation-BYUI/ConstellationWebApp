988896dfa45a5f2b9a8a9ef20678e87df385456b
RyanHARRIS18 <ryanpaulharris18@gmail.com>
2020-05-21 15:32:17 -0600

    Delete buttons work

---
+++ Controllers/UserProjectsController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ConstellationWebApp.Data;
+using ConstellationWebApp.Models;
+
+namespace ConstellationWebApp.Controllers
+{
+    public class UserProjectsController : Controller
+    {
+        private readonly ConstellationWebAppContext _context;
+
+        public UserProjectsController(ConstellationWebAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserProjects
+        public async Task<IActionResult> Index()
+        {
+            var constellationWebAppContext = _context.UserProjects.Include(u => u.Project).Include(u => u.User);
+            return View(await constellationWebAppContext.ToListAsync());
+        }
+
+        // GET: UserProjects/Details/5
+        public async Task<IActionResult> Details(string? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userProject = await _context.UserProjects
+                .Include(u => u.Project)
+                .Include(u => u.User)
+                .FirstOrDefaultAsync(m => m.UserID == id);
+            if (userProject == null)
+            {
+                return NotFound();
+            }
+
+            return View(userProject);
+        }
+
+        // POST: UserProjects/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserID,ProjectID")] UserProject userProject)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userProject);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProjectID"] = new SelectList(_context.Projects, "ProjectID", "Description", userProject.ProjectID);
+            ViewData["UserID"] = new SelectList(_context.User, "UserID", "Bio", userProject.UserID);
+            return View(userProject);
+        }
+
+        // POST: UserProjects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int projectID, string userID)
+        {
+           UserProject thisCollab =  _context.UserProjects.Where(i => i.ProjectID == projectID && i.UserID == userID).FirstOrDefault();
+            _context.UserProjects.Remove(thisCollab);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Projects");
+        }
+
+        private bool UserProjectExists(string id)
+        {
+            return _context.UserProjects.Any(e => e.UserID == id);
+        }
+    }
+}
