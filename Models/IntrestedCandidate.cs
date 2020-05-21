f651c2f9bac7a396551f51973bb65de69a27ee0b
RyanHARRIS18 <ryanpaulharris18@gmail.com>
2020-05-21 11:48:29 -0600

    Users can now input thier intrest in a posting and remove that intrest, update to the starred posting feature button as well

---
+++ Models/IntrestedCandidate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstellationWebApp.Models
+{
+    public class IntrestedCandidate
+    {
+        public int IntrestedCandidateID { get; set; }
+        public string UserID { get; set; }
+        public User User { get; set; }
+        public int PostingID { get; set; }
+        public Posting Posting { get; set; }
+    }
+}
