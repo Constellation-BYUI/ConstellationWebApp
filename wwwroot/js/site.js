c6115e7943384e21be31f58ff1e9dc8db0f619d0
Nwokolo Bueze <nwokolobueze@gmail.com>
2020-06-14 23:45:46 -0600

    Created new looks for Home page

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -25,39 +25,6 @@ function topFunction() {
 }
 
 
-// Slideshows
-var slideIndex = 1;
-
-function plusDivs(n) {
-    slideIndex = slideIndex + n;
-    showDivs(slideIndex);
-}
-
-function showDivs(n) {
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = x.length };
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[slideIndex - 1].style.display = "block";
-}
-
-showDivs(1);
-
-
-// Accordion
-function myFunction(id) {
-    var x = document.getElementById(id);
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-        x.previousElementSibling.className += " w3-theme-d1";
-    } else {
-        x.className = x.className.replace("w3-show", "");
-        x.previousElementSibling.className =
-            x.previousElementSibling.className.replace(" w3-theme-d1", "");
-    }
-}
 
 // Used to toggle the menu on smaller screens when clicking on the menu button
 function openNav() {
@@ -77,4 +44,14 @@ function openUser() {
     } else {
         x.className = x.className.replace(" w3-show", "");
     }
+}S
+
+
+function myFunction() {
+    var x = document.getElementById("stuffDrop");
+  if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+  } else {
+        x.className = x.className.replace(" w3-show", "");
+  }
 }
