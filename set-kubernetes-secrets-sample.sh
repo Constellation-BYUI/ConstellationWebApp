9bde2c0c18d7d131933fd3605d4064053ef70aa5
Sean Murdock <murdockse@byui.edu>
2020-06-18 11:30:43 -0600

    Add sample script

---
+++ set-kubernetes-secrets-sample.sh
@@ -0,0 +1,1 @@
+kubectl create secret generic app-settings --from-literal=connection-strings='{"ConstellationWebAppContext": "Server=34.75.4.1;User Id=sa;password=Gd6-tq%(uYd/3]BH;Database=constellation;MultipleActiveResultSets=true"}'
