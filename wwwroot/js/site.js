b363ce11f604b5b08bbc4712adaf6ba4149c8dcc
Nwokolo Bueze <nwokolobueze@gmail.com>
2020-06-30 12:17:08 -0600

    Added info button to all pages

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,12 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-//Posting adding show/hide
+//Hide or show page information
+$(document).ready(function () {
+    $('#button-info').click(function () {
+        $('article').toggleClass('display')
+    })
+})
 
 
 //Posting adding show/hide
