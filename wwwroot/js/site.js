bf92831798fd4e629e965ce0577a54616a58c793
Nwokolo Bueze <nwokolobueze@gmail.com>
2020-07-03 23:15:42 -0600

    made general changes to pages on contellation

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,11 +1,35 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+//BurgarMenu Control on Small screeens
+//NavBar, MyVolt, and avatar 
+$(document).ready(function () {
+    $('.menu-toggle').click(function () {
+        $('nav').toggleClass('active')
+    })
+})
+
+$(document).ready(function () {
+    $('#my-volt').click(function () {
+        $('.volt-down').toggleClass('show')
+    })
+})
+$(document).ready(function () {
+    $('.avatar').click(function () {
+        $('.avatar-down').toggleClass('show')
+    })
+})
+
+
 //Hide or show page information
 $(document).ready(function () {
     $('#button-info').click(function () {
-        $('article').toggleClass('display')
+        $('article').addClass('display')
+    });
+    $('body').click(function (){
+        $('article').removeClass('display')
     })
+   
 })
 
 
@@ -29,13 +53,6 @@ function add_collab() {
 
 }
 
-//Info button on all pages javascript
-var btn = document.getElementById('btn1');
-var p = document.getElementById('hidden-info-blip');
-
-btn.addEventListener('click', evt => {
-    p.classList.toggle('show-info');
-});
 
 //JavaScript to control the Scroll-To-Top Button
 //Get the button:
@@ -56,59 +73,4 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-
-// Slideshows
-var slideIndex = 1;
-
-function plusDivs(n) {
-    slideIndex = slideIndex + n;
-    showDivs(slideIndex);
-}
-
-function showDivs(n) {
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = x.length };
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[slideIndex - 1].style.display = "block";
-}
-
-showDivs(1);
-
-
-// Accordion
-function myFunction(id) {
-    var x = document.getElementById(id);
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-        x.previousElementSibling.className += " w3-theme-d1";
-    } else {
-        x.className = x.className.replace("w3-show", "");
-        x.previousElementSibling.className =
-            x.previousElementSibling.className.replace(" w3-theme-d1", "");
-    }
-}
-
-// Used to toggle the menu on smaller screens when clicking on the menu button
-function openNav() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
-}
-
-
-function openUser() {
-    var x = document.getElementById("navSetting");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
 }
